76461a83415641fc57ca647a96bceefee185eb2d
jack <62025739+1602077@users.noreply.github.com>
2023-06-23 21:21:28 +0100

    refactor: hmac secret for url encoding to cookie

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,14 +1,13 @@
+use actix_web::cookie::Cookie;
 use actix_web::error::InternalError;
 use actix_web::{web, HttpResponse, ResponseError};
-use hmac::{Hmac, Mac};
 use reqwest::header::LOCATION;
 use reqwest::StatusCode;
-use secrecy::{ExposeSecret, Secret};
+use secrecy::Secret;
 use sqlx::PgPool;
 
 use crate::authentication::{validate_credentials, AuthError, Credentials};
 use crate::routes::error_chain_fmt;
-use crate::startup::HmacSecret;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -19,7 +18,6 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    secret: web::Data<HmacSecret>,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let creds = Credentials {
         username: form.0.username,
@@ -47,21 +45,9 @@ pub async fn login(
                     LoginError::UnexpectedError(e.into())
                 }
             };
-            let query_string =
-                format!("error={}", urlencoding::Encoded::new(e.to_string()));
-            let hmac_tag = {
-                let mut mac = Hmac::<sha2::Sha256>::new_from_slice(
-                    secret.0.expose_secret().as_bytes(),
-                )
-                .unwrap();
-                mac.update(query_string.as_bytes());
-                mac.finalize().into_bytes()
-            };
             let response = HttpResponse::SeeOther()
-                .insert_header((
-                    LOCATION,
-                    format!("/login?{}&tag={:x}", query_string, hmac_tag),
-                ))
+                .insert_header((LOCATION, "/login"))
+                .cookie(Cookie::new("_flash", e.to_string()))
                 .finish();
 
             Err(InternalError::from_response(e, response))
@@ -71,7 +57,7 @@ pub async fn login(
 
 #[derive(thiserror::Error)]
 pub enum LoginError {
-    #[error("authentication failed")]
+    #[error("Authentication failed")]
     AuthError(#[source] anyhow::Error),
 
     #[error("Something went wrong")]
