291e9f1c625b3fe727c7f9bc786f7932b4cb899b
jack <62025739+1602077@users.noreply.github.com>
2023-06-24 22:27:29 +0100

    feat: create seed user

---
+++ migrations/20230624212239_seed_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'bf9bc63f-d7e8-4cbc-a84c-680472904c87',
+    'root',
+    -- PHC string corresponding to password of 'password'.
+    '$argon2id$v=19$m=15000,t=2,p=1$/GGfijn0VGeBRJf7P1I5Kw$n8cVlFu1xnzc4GAsaT00SNw1jmF4iC9wtsjEMXZ1fyw'
+);
