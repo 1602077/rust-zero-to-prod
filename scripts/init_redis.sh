f79deb6a4183608d534e423dff9020708ec733ba
jack <62025739+1602077@users.noreply.github.com>
2023-06-24 11:28:53 +0100

    feat!: inital redis session management

---
+++ scripts/init_redis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eoa pipefail
+
+RUNNING_CONTAINER=$(docker ps --filter 'name=redis' --format '{{.ID}}')
+
+[[ "$RUNNING_CONTAINER" ]] && (echo "killing $RUNNING_CONTAINER" && docker kill $RUNNING_CONTAINER)
+
+docker run \
+	-p "6379:6379" \
+	-d \
+	--name "redis_$(date '+%s')" \
+	redis:7
+
+echo >&2 "\nredis is running"
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -108,6 +108,17 @@ impl TestApp {
             .await
             .unwrap()
     }
+
+    pub async fn get_admin_dashboard(&self) -> String {
+        self.api_client
+            .get(&format!("{}/admin/dashboard", &self.address))
+            .send()
+            .await
+            .expect("failed to execute request")
+            .text()
+            .await
+            .unwrap()
+    }
 }
 
 impl TestUser {
